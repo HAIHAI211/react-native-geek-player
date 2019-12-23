e74d7857bb095955f309d34055c2889fe8e9a015
sunzheng <haihai211>
2019-12-23 13:18:59 +0800

    ios init

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+import {NativeModules, Platform} from 'react-native'
+const native = Platform.OS === 'ios' ?  NativeModules.VideoPlayer : NativeModules.SuperPlayerModule
+const play = (url) => {
+  if (!url) {
+    console.warn('视频没有url')
+    return
+  }
+  const finalUrl = url.startsWith('http') ? url : 'http://' + url
+  native && native.play && native.play(finalUrl)
+}
 
-import { NativeModules } from 'react-native';
-
-const { RNPlayer } = NativeModules;
-
-export default RNPlayer;
+export default play
